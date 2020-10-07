9528186c154802e162de948ef58387b3b7b72839
Manuel Portela <manuelportela@Manuels-MBP.mshome.net>
2020-10-07 13:56:35 -0400

    Some-User-routes

---
+++ controllers/user.js
@@ -0,0 +1,19 @@
+const changeUsername = () => {
+  return null;
+};
+const changePassword = () => {
+  return null;
+};
+const urlList = () => {
+  return null;
+};
+const extendSession = () => {
+  return null;
+};
+
+module.exports = {
+  changeUsername,
+  changePassword,
+  urlList,
+  extendSession
+};
